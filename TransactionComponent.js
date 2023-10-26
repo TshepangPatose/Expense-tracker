5a2f2ea59d4265932554da199dbb863195e8dfe2
TshepangPatose <140078310+TshepangPatose@users.noreply.github.com>
2023-10-26 14:18:56 +0200

    Add files via upload

---
+++ TransactionComponent.js
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  dispaly: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 100%;
+  gap: 10px;
+  font-weight: bold;
+  & input {
+    padding: 10px 12px;
+    border-radius: 12px;
+    background: #e6e8e9;
+    border: 1px solid #e6e8e9;
+    outline: none;
+    width: 100%;
+  }
+`;
+const Cell = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 15px;
+  font-size: 14px;
+  border-radius: 2px;
+  align-items: center;
+  font-weight: normal;
+  width: 100%;
+  justify-content: space-between;
+  border: 1px solid #e6e8e9;
+  border-right: 4px solid R{(props) => (props.isExpense ? "red" : "green")};
+`;
+
+const TransactionCell = (props) => {
+  return(
+    <Cell isExpense={props.payload?.type === "EXPENSE"}>
+      <span>{props.payload.desc}</span>
+      <span>R{props.payload.amount}</span>
+    </Cell>
+  );
+};
+const TransactionComponent = (props) => {
+  const [searchText, updateSearchText] = useState("");
+  const [filteredTransaction, updateTxn] = useState(props.transactions);
+  const filterData = (searchText) =>{
+    if (!searchText || !searchText.trim().length){
+      updateTxn(props.transactions);
+      return;
+    }
+    let txn = [...props.transactions];
+    txn = txn.filter((payload)=> 
+      payload.desc.toLowerCase().includes(searchText.toLowerCase().trim())
+    );
+    updateTxn(Txn);
+  };
+
+  useEffect(() => filterData(searchText), [props.transactions]);
+
+  return(
+    <Container>
+      Transactions
+      <input 
+        placeholder="Search" 
+        value={searchText} 
+        onChange={(e)=> {
+          updateSearchText(e.target.value);
+          filterData(e.target.value);
+        }}
+      />
+      {filteredTransaction?.length
+        ? filteredTransaction.map((payload)=>(
+          <TransactionCell payload={payload} />
+          ))
+        :""}
+    </Container>
+  );
+};
+export default TransactionComponent;
--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import OverviewComponent from "./OverviewComponent";
+import TransactionComponent from "./TransactionComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Container = styled.div`
+  dispaly: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0 10px;
+  font-family: Montserrat;
+  width: 360px;
+`;
+
+const  HomeComponent = (props) => {
+    const [transactions, updateTransaction] = useState([]);
+    const [expense, updateExpense] = useState(0);
+    const [income, updateIncome] = useState(0);
+    
+    const addTransaction = (payload) => {
+      const transactionArray = [...transactions];
+      transactionArray.push(payload);
+      updateTransaction(transactionArray);
+    };
+
+    const calculateBalance = () => {
+      let exp = 0;
+      let inc = 0;
+      transactions.map((payload) => {
+        payload.type === "EXPENSE"
+          ? (exp = exp + payload.amount)
+          : (inc = inc + payload.amount);
+      });
+      updateExpense(exp);
+      updateIncome(inc);
+    };
+
+    useEffect(() => calculateBalance(), [transactions]);
+
+    return (
+      <Container>
+        <OverviewComponent 
+          addTransaction={addTransaction} 
+          expense={expense} 
+          income={income}
+        />
+        <TransactionComponent transactions={transactions} />
+      </Container>
+    );
+};
+export default HomeComponent;
